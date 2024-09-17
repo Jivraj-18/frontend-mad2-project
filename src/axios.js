c67d49a03303beb743ea8be30d4a78f499382503
Jivraj Singh Shekhawat <123666967+Jivraj-18@users.noreply.github.com>
2024-09-17 20:47:04 +0000

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosFetch = axios.create({
-    baseURL: 'http://127.0.0.1:5000',
+    baseURL: 'https://my-mad2-project.onrender.com/',
 })
 
 const authToken = localStorage.getItem('authToken');
